97b52e5a6f06e3987a3f0121dc9c49d6fb4b461c
Veronica Ludemann <veronicaludemann67@gmail.com>
2025-11-25 18:42:11 -0300

    conflictos!

--- cypress/e2e/casosClient/loginCte.cy.js
+++ cypress/e2e/casosClient/loginCte.cy.js
@@ -3,11 +3,13 @@ describe('Logout - Caso exitoso', () => {
     beforeEach(() => {
         cy.visit('/auth/login')
         cy.fixture('registerCteEvent.json').then((data) => {
-        const cliente = {...data.loginOk}
-        cy.LoginExito(cliente)//llamo al commands
+            cy.wrap(data.loginOk).as('cliente')
         })
     })
 
+    it('El usuario puede cerrar sesión correctamente', function () {
 
+        cy.LoginExito(this.cliente)  
+        cy.logout()
+    })
 })
-
